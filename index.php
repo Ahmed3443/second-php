53860b60037788d577a2a905f27ded412b053e96
Ahmed3443 <83243953+Ahmed3443@users.noreply.github.com>
2021-10-08 19:47:47 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,266 @@
+
+ <?php 
+ 
+ /*
+$text = "ahmed" ;
+echo $text;
+$number = 76767; 
+echo$number;
+$float_number = 7667.545 ;
+echo $float_number;
+echo "<h1>allah</h1>" ;
+$boolean = false;
+$name = null ;
+$name = "alaah<p></p>";
+echo $name ; 
+$numbers = array(20220,454545,5454,54545,54545,54545,5454);
+echo $numbers[0];
+ 
+$data = array(1,1,1,255);
+$check = is_array($data);
+var_dump($check)      // عرض البوليين 
+
+$number = 200;
+echo $number;
+
+
+
+$strlen = strlen ($text);
+echo $strlen ;
+
+$text = "i love egypt";
+$text_count_word = str_word_count($text);
+echo$text_count_word;
+ $text_strreve = strrev($text);
+ echo$text_strreve ;
+ 
+
+$text_strreve = strrev($text);
+echo $text_strreve ;
+*/
+// $text = "i      love     egypt";
+//$text_strops = strpos($text,"ahmed");
+//var_dump($text_strops)
+//str_replace
+
+// $text_replace = str_replace("love","  " ,$text);
+// echo $text_replace ;
+//trim()  حذف الفراغات 
+// $text_trim = trim($text);
+// echo $text_trim ;
+
+// العوامل الرياضية    arthimatic operators 
+// $number1 = 100 ;
+// $number2 = 30 ;
+// $result = 12 ** 2 ;
+// echo $result ; 
+
+
+
+// 3awaml el e7ala  Assigment operators   += , -= , *= , /= , %= 
+
+// $number1 = 14 ;
+// $number2 = 30 ;
+// $number2 %= $number1 ;
+// echo $number2 ; 
+
+/////////////////////////////////////////
+
+// if statement 
+
+// $number1 = 4;
+// $number2 = 7 ; 
+
+// if ($number1 <= 8) {
+//     echo "done ";
+// }
+// elseif () {
+//     # code...
+// }
+// elseif (condition) {
+//     # code...
+// }
+// else{
+//     echo "not found " ; 
+// }
+
+// $point_student = "70 ghggh" ; 
+// if ($point_student>=90){
+//     echo "excellent ";
+
+// }
+// elseif ($point_student>=70 ) {
+//     echo "verry good";
+// }
+// elseif ($point_student>=50 ) {
+//     echo " good";
+// }
+// else {
+//     echo "not passed ";
+// }
+
+
+
+// logical operators   العوامل المنطقية 
+
+//( and  && . or|| .  xor  . ! )
+  
+// $username = "ahmedkhalid" ;
+// $password = "1234" ;
+// $email = "test@gmail.com";
+// if (($username == "ahmedkhalid" || $email =="test@gmail.com" )&& $password == "1234" ) 
+// {
+//     echo "success" ; 
+//     # code...
+// }
+// else 
+// {
+//     echo "faild" ;
+
+// }
+// if (!($username == "ahmedkhalid" ))
+// {
+//     echo "hello";
+// }
+// else {
+//     echo "not found ";
+// }
+
+// switch case   test variable 
+// $year = 2021;
+// switch( $year)
+// {
+//         case  "2020":
+//         echo"2020 this year" ;
+//         break ;
+//         case 2020 :
+//         echo "year is 2020 integer" ;
+//         break ;
+//         default :
+//         echo "لم يتطابق مع اي احتمالات " ;
+//         break ;
+// }
+
+//    $color = 0; 
+//    switch ($color) {
+//        case 1:
+//            echo "<style> body{ background : red ; color : white ;
+//         }</style>" ;
+//            echo "<h1>the first prop</h1>";
+//            break;
+//            case 2:
+//             echo "<style> body{ background : black ; color : white ;
+//             }</style>" ;
+//                echo "<h1>the second prop</h1>";
+//             break;case 3:
+//                 echo "<style> body{ background : blue ; color : white ;
+//                 }</style>" ;
+//                    echo "<h1>the third prop</h1>";
+//                 break;
+       
+//        default:
+//        echo "<h1>not found </h1>"; 
+//            break;
+//    }
+
+
+
+//.   +
+//.=   +=
+// $firstName = "ahmed";
+// $lastNAme = "khalid" ;
+// $name = $firstName ."  " . $lastNAme ;
+// echo $name ; 
+// $number1 = 10 ;
+// $number2 = 5 ;
+// echo $number1 .= $number2 ;
+   
+
+
+// loops    while
+
+// $counter = 1 ;
+// while ($counter<=10 )
+// {   
+//      $result = $counter * 9 ; 
+//     echo "<h1>$counter * 9 = $result</h1>";
+//  ++$counter;
+// }
+  
+
+// for
+// for($counter = 20; $counter>= 20 ; --$counter)
+// {
+   
+//          echo "<h1>$counter * 9 = " . $counter*9 ."</h1>";
+//          echo"<h1>$counter</h1>" ;
+// }
+
+
+
+//do while
+// $counter = 1 ;
+// do{
+//     echo " <h1>done $counter </h1>" ;
+// ++$counter ;
+// }while($counter<=10);
+
+
+// continue and break    .  loop 
+  // $counter = 1 ; 
+//   while($counter <=10)
+//   {    
+//       if ($counter == 6 || $counter == 3) {
+//           ++$counter;
+//           continue;
+//       }
+//     if ($counter == 5) {
+//         break;
+//     }
+//       echo "<div>the number of reprater $counter  </div>" ;
+//       ++$counter ;
+//   }
+// $counter = 1 ;
+//  for ($counter=1; $counter <=10 ; ++$counter) { 
+//      if ($counter ==5) {
+//          break;
+         
+//      }
+//      echo "<div>the number of reprater $counter  </div>" ;
+//  }
+
+// $i = 1   ;
+// for ($i=1; $i <=10 ; $i++) { 
+//     if ($i == 5) {
+//         break ;
+//     }
+//     echo "<div>the number of reprater of  i  $i  </div>" ;
+// }
+ 
+?>
+
+//get &&post method
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
